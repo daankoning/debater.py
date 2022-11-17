304df3be6c31c05184c1117aacf847326ea0990f
Daan Koning <daanolivierkoning@gmail.com>
2022-11-17 16:02:32 +0100

    Ensured rst inline code formatting works

--- src/debaterpy/utils.py
+++ src/debaterpy/utils.py
@@ -1,4 +1,4 @@
-"""Various utilities to aid in the manipulation and analysis of the objects in `debaterpy.structures`."""
+"""Various utilities to aid in the manipulation and analysis of the objects in ``debaterpy.structures``."""
 from .structures import *
 from typing import Generator, Callable
 import io
@@ -7,19 +7,19 @@ import csv
 
 def get_all_rounds(record: Record, function: Callable[[Tournament, Round], bool] = lambda x, y: True)\
 		-> Generator[Round, None, None]:
-	"""Gets all the rounds `record` for which `function` returns `True`.
+	"""Gets all the rounds ``record`` for which ``function`` returns ``True``.
 
-	This allows easier access to all the data such as for analysis of speaks or winrates. The `function` argument
-	behaves similarly to Python's built-in `filter`, it only allows entries for which `function(x, y)` is `True`. For
+	This allows easier access to all the data such as for analysis of speaks or winrates. The ``function`` argument
+	behaves similarly to Python's built-in ``filter``, it only allows entries for which ``function(x, y)`` is ``True``. For
 	example, the following code will get all rounds in BP (which might be useful for splitting up analyses by format):
 
 		>>> get_all_rounds(record, lambda x, y: x.format == "BP")
 		<generator object get_all_rounds at 0x102861e40>
 
 	Args:
-		record: An instance of `structures.Record` from which the rounds will be extracted.
+		record: An instance of ``structures.Record`` from which the rounds will be extracted.
 		function:  A callable returning a boolean which needs to be true for a record to be included. Receives instances
-			of `structures.Tournament` and `structures.Round`as arguments. Will default to including all rounds."""
+			of ``structures.Tournament`` and ``structures.Round``as arguments. Will default to including all rounds."""
 	for tournament in record.tournaments:
 		for round in tournament.rounds:
 			if function(tournament, round):
@@ -29,8 +29,8 @@ def get_all_rounds(record: Record, function: Callable[[Tournament, Round], bool]
 def merge_records(*args: Record) -> Record:
 	"""Merges all the records passed, tournament-wise.
 
-	Combines multiple records by their tournaments, creating one new `Record` object containing all the tournaments in
-	the original records. Duplicate tournaments are identified by `Tournament.tournament_name`, meaning the same name
+	Combines multiple records by their tournaments, creating one new ``Record`` object containing all the tournaments in
+	the original records. Duplicate tournaments are identified by ``Tournament.tournament_name``, meaning the same name
 	will never show up twice in the record.
 
 	If a tournament is in multiple records only the version of the tournament in the record that is passed first will
@@ -52,7 +52,7 @@ def merge_records(*args: Record) -> Record:
 
 
 def generate_csv(record: Record, target: io.TextIOWrapper):
-	"""Writes a CSV representation of `record` to `target`."""
+	"""Writes a CSV representation of ``record`` to ``target``."""
 	writer = csv.writer(target, delimiter=",")
 	writer.writerow(["tournament_name", "format", "broke", "break_categories", "name", "outround",
 					 "outround_category", "prepped", "date", "date_string", "topics", "motion", "infoslide", "side", "half",
